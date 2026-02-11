28ec6e53e1cadc7be5a6c7a6bacd8b506c5b904d
unnikrishnan06 <unnikrishnan44404@gmail.com>
2026-02-10 23:49:56 -0500

    Added LibraryManager

---
+++ LibraryManager.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LibraryManager {
+
+    public static void main(String[] args) {
+
+        // Scanner for user input
+        Scanner input = new Scanner(System.in);
+
+        // ArrayList to store Book objects
+        ArrayList<Book> bookList = new ArrayList<>();
+
+        int choice = 0;
+
+        // Menu loop
+        while (choice != 5) {
+
+            System.out.println("\n----- Library Menu -----");
+            System.out.println("1. Add a new book");
+            System.out.println("2. Display all books");
+            System.out.println("3. Display available books");
+            System.out.println("4. Search books by author");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = input.nextInt();
+            input.nextLine(); 
+        }
+
+        input.close();
+    }
+}
