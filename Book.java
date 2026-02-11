d11d470c7baecf28e918e316b4ff6083a792ff1a
unnikrishnan06 <unnikrishnan44404@gmail.com>
2026-02-10 23:36:00 -0500

    added contructor and getter

---
+++ Book.java
@@ -0,0 +1,35 @@
+
+public class Book {
+
+    // Private variables 
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookISBN;
+    private boolean isAvailable;
+
+
+    // Constructor
+    public Book(String title, String author, String isbn) {
+        bookTitle = title;
+        bookAuthor = author;
+        bookISBN = isbn;
+        isAvailable = true; // book is available by default
+    }
+
+    // Getter methods
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+}
